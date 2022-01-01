ec66fd6c4b0c71470d9eb77e204694ced3b7b96f
tao luo <taoluo@usc.edu>
2022-01-01 14:52:03 -0800

    add tests for curry

--- curry/curry.test.js
+++ curry/curry.test.js
@@ -1,9 +1,21 @@
 import curry from "./curry.js";
 
-test("test 1", () => {
-  const add = (a, b, c) => {
-    return a + b + c;
-  };
+const add = (a, b, c) => {
+  return a + b + c;
+};
 
+test("test 1", () => {
   expect(curry(add)(1)(2)(3)).toBe(6);
+});
+
+test("test 2", () => {
+  expect(curry(add)(13)(-13)(400)).toBe(400);
+});
+
+test("test 3", () => {
+  expect(typeof curry(add)).toBe("function");
+});
+
+test("test 4", () => {
+  expect(typeof curry(add)(1)(2)).toBe("function");
 });
