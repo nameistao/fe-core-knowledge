e142fbd1485e5511323bd46587bd78036d29a404
tao luo <taoluo@usc.edu>
2022-01-01 22:33:58 -0800

    add groupBy

---
+++ groupBy/groupBy.js
@@ -0,0 +1,13 @@
+const groupBy = (arr, size = 1) => {
+  const output = [];
+  const temp = [];
+  arr.forEach((elem, index) => {
+    temp.push(elem);
+    if (temp.length === size || index === arr.length - 1) {
+      output.push([...temp]);
+      temp.length = 0;
+    }
+  });
+
+  return output;
+};
