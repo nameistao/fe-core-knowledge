90e35bf7cab3dcf6f37fe3abab9d7214bc7a437a
tao.luo <taoluo@usc.edu>
2022-02-28 21:46:30 -0800

    add stopPropagation

---
+++ events/stopPropagation/stopPropagation.js
@@ -0,0 +1,40 @@
+const gparent = document.querySelector(".gparent");
+const parent = document.querySelector(".parent");
+const child = document.querySelector(".child");
+
+gparent.addEventListener(
+  "click",
+  function (e) {
+    alert("gparent capture");
+  },
+  { capture: true }
+);
+
+parent.addEventListener(
+  "click",
+  function (e) {
+    alert("parent capture");
+  },
+  { capture: true }
+);
+
+child.addEventListener(
+  "click",
+  function (e) {
+    alert("child capture");
+  },
+  { capture: true }
+);
+
+child.addEventListener("click", function (e) {
+  alert("child bubble");
+});
+
+parent.addEventListener("click", function (e) {
+  e.stopPropagation();
+  alert("parent bubble");
+});
+
+gparent.addEventListener("click", function (e) {
+  alert("gparent bubble");
+});
