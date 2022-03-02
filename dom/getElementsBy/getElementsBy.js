a7ab8d9881e47979cc6afff4dd4a42610782cfca
tao.luo <taoluo@usc.edu>
2022-03-01 21:10:47 -0800

    add getElementsBy

---
+++ dom/getElementsBy/getElementsBy.js
@@ -0,0 +1,8 @@
+const input1 = document.getElementById("input1");
+console.log(input1);
+
+const labels = Array.from(document.getElementsByClassName("label"));
+console.log(labels);
+
+const inputs = Array.from(document.getElementsByTagName("input"));
+console.log(inputs);
