0393b004c992d2d5e07690ae02d4a45c53d667d1
tao.luo <taoluo@usc.edu>
2022-03-02 12:07:54 -0800

    move fragment

--- dom/addRemoveElements/fragment/fragment.js
+++
@@ -1,9 +0,0 @@
-const list = document.querySelector("#list");
-const fragment = new DocumentFragment();
-const movies = ["mummy", "avengers", "apollo 13", "pet semetary"];
-movies.forEach((movie) => {
-  let li = document.createElement("li");
-  li.innerText = movie;
-  fragment.append(li);
-});
-list.append(fragment);
