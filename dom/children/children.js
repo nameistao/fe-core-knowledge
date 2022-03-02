dd4baabbc696cfe7ec458240e30e4ea1b2bd3864
tao.luo <taoluo@usc.edu>
2022-03-01 21:11:02 -0800

    add closest and children

---
+++ dom/children/children.js
@@ -0,0 +1,9 @@
+const childNodes = Array.from(document.querySelector("ul").childNodes);
+const firstChild = document.querySelector("ul").firstChild;
+const lastChild = document.querySelector("ul").lastChild;
+const children = Array.from(document.querySelector("ul").children);
+
+console.log(childNodes);
+console.log(firstChild);
+console.log(lastChild);
+console.log(children);
---
+++ dom/siblings/siblings.js
@@ -0,0 +1,7 @@
+const two = document.querySelector("#two");
+
+console.log(two.previousSibling);
+console.log(two.nextSibling);
+
+console.log(two.previousElementSibling);
+console.log(two.nextElementSibling);
